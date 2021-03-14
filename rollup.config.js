3908214dcc0799d7f550793415689f9bf31c5922
rayat69 <rayathossain49@gmail.com>
2021-03-14 15:29:00 +0600

    bug fix 4

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
 import { src, main } from "./package.json";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import Json from "@rollup/plugin-json";
 import { uglify } from "rollup-plugin-uglify";
 import gzipPlugin from "rollup-plugin-gzip";
-import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export default {
   input: src,
